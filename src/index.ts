ecd1a7c78791b0a145be7db519a5a65345c18c1b
Fu Diwei <fudiwei@sina.com>
2023-11-08 10:11:03 +0800

    init commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+﻿import { setLicenseKey } from '@surely-vue/table';
+import { MD5 as md5 } from 'crypto-js';
+import { encode as encodeBase64 } from 'js-base64';
+
+export const hackLicenseKey = (hostname?: string) => {
+    const SURELY_LICENSE_KEY = encodeBase64(`ORDER:00001,EXPIRY=33227712000000,DOMAIN=${hostname || location.hostname},KEYVERSION=1`);
+    const SURELY_LICENSE_SIGN = md5(SURELY_LICENSE_KEY).toString().toLowerCase();
+    setLicenseKey(`${SURELY_LICENSE_SIGN}${SURELY_LICENSE_KEY}`);
+}
