862d2c99e30b4b307f8aaa996c0a81d510ac13b7
Fu Diwei <fudiwei@sina.com>
2024-03-14 01:46:39 +0800

    refactor: hackLicenseKey options

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@
 import { MD5 as md5 } from 'crypto-js';
 import { encode as encodeBase64 } from 'js-base64';
 
+interface HackLicenseKeyOptions {
+    hostname?: string;
+}
+
 /**
  * 破解 \@surely-vue/table 授权。
- * @param {String} [hostname] 网站域名（默认值：`location.hostname`）。
+ * @param {HackLicenseKeyOptions} [options] 配置项。
+ * @param {String} [options.hostname] 授权域名（默认值：`location.hostname`）。
  */
-export const hackLicenseKey = (hostname = location.hostname) => {
-    const key = encodeBase64(`ORDER:00001,EXPIRY=33227712000000,DOMAIN=${hostname},KEYVERSION=1`);
+export const hackLicenseKey = (options?: HackLicenseKeyOptions) => {
+    const domain = options?.hostname ?? location.hostname;
+    const key = encodeBase64(`ORDER:00001,EXPIRY=33227712000000,DOMAIN=${domain},KEYVERSION=1`);
     const sign = md5(key).toString().toLowerCase();
     setLicenseKey(`${sign}${key}`);
 };
