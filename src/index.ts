7f0e52136d02e3b315c007b2d737ada26325aa19
Fu Diwei <fudiwei@sina.com>
2024-03-13 23:36:27 +0800

    style: config code linter

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ import { encode as encodeBase64 } from 'js-base64';
  * 破解 \@surely-vue/table 授权。
  * @param {String} [hostname] 网站域名（默认值：`location.hostname`）。
  */
-export const hackLicenseKey = (hostname?: string) => {
-    const SURELY_LICENSE_KEY = encodeBase64(`ORDER:00001,EXPIRY=33227712000000,DOMAIN=${hostname || location.hostname},KEYVERSION=1`);
-    const SURELY_LICENSE_SIGN = md5(SURELY_LICENSE_KEY).toString().toLowerCase();
-    setLicenseKey(`${SURELY_LICENSE_SIGN}${SURELY_LICENSE_KEY}`);
-}
+export const hackLicenseKey = (hostname = location.hostname) => {
+    const key = encodeBase64(`ORDER:00001,EXPIRY=33227712000000,DOMAIN=${hostname},KEYVERSION=1`);
+    const sign = md5(key).toString().toLowerCase();
+    setLicenseKey(`${sign}${key}`);
+};
