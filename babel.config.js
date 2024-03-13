2590818fbf33396d07a90a72477723d4aed51b43
Fu Diwei <fudiwei@sina.com>
2024-03-14 01:47:05 +0800

    build: config rollup

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+﻿export default {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: 'chrome>=79, safari>=13.1, firefox>=72, not IE 11'
+            }
+        ]
+    ]
+};
---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+﻿import path from 'node:path';
+import deepmerge from 'deepmerge';
+import { defineConfig } from 'rollup';
+import rollupPluginBabel from '@rollup/plugin-babel';
+import rollupPluginCommonJS from '@rollup/plugin-commonjs';
+import rollupPluginTerser from '@rollup/plugin-terser';
+import rollupPluginTypeScript from '@rollup/plugin-typescript';
+
+function configureMain() {
+    const baseConfig = () => {
+        return defineConfig({
+            input: path.resolve('./src/index.ts'),
+
+            plugins: [
+                rollupPluginBabel({
+                    babelHelpers: 'bundled',
+                    extensions: ['.js', '.ts'],
+                    exclude: /[\\/]node_modules[\\/]/
+                }),
+
+                rollupPluginCommonJS(),
+
+                rollupPluginTypeScript({
+                    tsconfig: path.resolve('./tsconfig.json')
+                }),
+
+                rollupPluginTerser()
+            ]
+        });
+    };
+
+    return [
+        // dist/index.cjs.js
+        deepmerge(
+            baseConfig(),
+            defineConfig({
+                output: {
+                    format: 'cjs',
+                    dir: path.resolve('./dist'),
+                    exports: 'named',
+                    preserveModules: true,
+                    entryFileNames: '[name].cjs.js'
+                }
+            })
+        ),
+        // dist/index.esm.js
+        deepmerge(
+            baseConfig(),
+            defineConfig({
+                output: {
+                    format: 'esm',
+                    dir: path.resolve('./dist'),
+                    exports: undefined,
+                    preserveModules: true,
+                    entryFileNames: '[name].esm.js'
+                }
+            })
+        )
+    ];
+}
+
+export default [...configureMain()];
