834087d8acc4f0a8d9c1ab81a465594f36a213e9
Fu Diwei <fudiwei@sina.com>
2024-03-13 23:31:52 +0800

    chore: upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
 import { MD5 as md5 } from 'crypto-js';
 import { encode as encodeBase64 } from 'js-base64';
 
+/**
+ * 破解 \@surely-vue/table 授权。
+ * @param {String} [hostname] 网站域名（默认值：`location.hostname`）。
+ */
 export const hackLicenseKey = (hostname?: string) => {
     const SURELY_LICENSE_KEY = encodeBase64(`ORDER:00001,EXPIRY=33227712000000,DOMAIN=${hostname || location.hostname},KEYVERSION=1`);
     const SURELY_LICENSE_SIGN = md5(SURELY_LICENSE_KEY).toString().toLowerCase();
